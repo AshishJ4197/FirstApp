df9138da2c064843f5077ea5e16ee1f99b0db537
Ashish <=ashujaladi5995@gmail.com>
2024-06-07 11:42:53 +0530

    First Commit

---
+++ app/src/main/java/com/example/newapp/kotlinexs/Example.kt
@@ -0,0 +1,7 @@
+package com.example.newapp.kotlinexs
+
+fun main(){
+    println("Ashish is great")
+    var myStudent: Student = Student("abdul",123,"bangalore")
+    println(myStudent.name+ " " +myStudent.age)
+}
---
+++ app/src/main/java/com/example/newapp/kotlinexs/Student.kt
@@ -0,0 +1,3 @@
+package com.example.newapp.kotlinexs
+
+class Student(var name: String, var age: Int, var postalAddress: String)
